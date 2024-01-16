48879e6c178c30bbe30447982decfd0e49f74313
sajad <sajjad.dadepardaz@gmail.com>
2024-01-16 12:04:54 +0330

    Adding dynamic behaviour to task constructor.

---
+++ venv/py/modify_variable.py
@@ -0,0 +1,61 @@
+{
+    "id":24,
+    "items": [
+        {
+            "vairable_name": "my_var",
+            "value": {
+                "row1": {
+                    "label": "Indicator"
+                },
+                "row2": {
+                    "name": "RSI",
+                    "description": "this is RSI indicator"
+                },
+                "params": [
+                    {
+                        "optionName": "symbol",
+                        "value": {
+                            "value": 14
+                        }
+                    },
+                    {
+                        "optionName": "timeframe",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    },
+                    {
+                        "optionName": "period",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    },
+                    {
+                        "optionName": "applied_price",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    },
+                    {
+                        "optionName": "buy_threshold",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    },
+                    {
+                        "optionName": "sell_threshold",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    },
+                    {
+                        "optionName": "shift",
+                        "value": {
+                            "value": "PRICE_CLOSE"
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
