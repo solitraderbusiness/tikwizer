fd5549bab0e76ee62f20dcc4cd63ca441ef1e08b
Sajjad <sajjad.dadepardaz@gmail.com>
2024-01-16 16:57:09 +0330

    In the middle of adding value and modify variable

--- venv/py/daw2.py
+++ venv/py/daw2.py
@@ -1,15 +1,41 @@
 
+
+# I wrote this script to help me with updating initializer files.
+
+
+
+
 import os
 import path_root
+import json
 
 path = path_root.get()
 folder_path = path + "/contents/indicators/"
 folder_list = [folder for folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, folder))]
 
 for folder in folder_list:
-    mpath = folder_path + folder
+    mpath = folder_path + folder + "/"
+    with open(mpath + "initializer.json") as file:
+        if file:
+            text = file.read()
+            dic = json.loads(text)
+            init = dic.get("initializer")
+            lines = init.split("\n")
 
-    print(mpath)
+            for i in range(len(lines)):
+                lines[i] = lines[i] +"\n"
+
+            mdic = {}
+            mdic["initializer"] = dic.get("initializer")
+            mdic["initializer_split"] = lines
+            mdic["variable_name"] = dic.get("variable_name")
+            print(json.dumps(mdic))
+            # with open(mpath + "initializer.json", "w") as result_file:
+            #     result_file.write( json.dumps(mdic))
+
+
+
+
 
 
 # print(folder_list)
