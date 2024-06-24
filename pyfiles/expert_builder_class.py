ace92580056ad9d6b295565e74a8e485b4fc9c93
Sajjad <sajjad.dadepardaz@gmail.com>
2024-06-24 17:00:15 +0330

    End day commit

--- pyfiles/expert_builder_class.py
+++ pyfiles/expert_builder_class.py
@@ -1161,5 +1161,6 @@ class ExpertBuilder:
             return self.value_class_constructor.get_class(row2, params, id_val, self.data.get("constants"),
                                                           self.data.get("variables"))
         elif row1 == "object-on-the-chart":
-            return self.object_on_the_chart_class_constructor.get_class(row2, params, id_val, self.data.get("constants"),
+            return self.object_on_the_chart_class_constructor.get_class(row2, params, id_val,
+                                                                        self.data.get("constants"),
                                                                         self.data.get("variables"))
