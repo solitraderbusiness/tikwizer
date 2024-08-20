baf6b9978e30fde252cf4877d76a38c4f25b8064
Sajjad <sajjad.dadepardaz@gmail.com>
2024-08-20 11:48:25 +0330

    Auto versioning had a minor fix.

--- pyfiles/expert_builder_class.py
+++ pyfiles/expert_builder_class.py
@@ -34,7 +34,7 @@ def get_version():
         if version_file:
             lines = version_file.read().split('\n')
             commented_lines = ['//' + line for line in lines]
-            return "\n\n\n".join(commented_lines)
+            return "\n\n\n" + '\n'.join(commented_lines)
 
 
 def handle_const_var_value(data):
